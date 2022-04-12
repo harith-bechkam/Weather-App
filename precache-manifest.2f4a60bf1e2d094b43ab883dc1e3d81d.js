53eb01807a70f5acb5eff1c7eed4d58407ffe6d5
harith <harith.kv47@gmail.com>
2022-04-12 09:18:52 +0530

    Updates

---
+++ precache-manifest.2f4a60bf1e2d094b43ab883dc1e3d81d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "473e0a85ed77850a669654fdbb7039eb",
+    "url": "/Weather-App/index.html"
+  },
+  {
+    "revision": "dffea15b0f20d5373d3a",
+    "url": "/Weather-App/static/css/2.261a0adc.chunk.css"
+  },
+  {
+    "revision": "ac4bb356dd45bf60bafd",
+    "url": "/Weather-App/static/css/main.d268e6be.chunk.css"
+  },
+  {
+    "revision": "dffea15b0f20d5373d3a",
+    "url": "/Weather-App/static/js/2.9b0d9541.chunk.js"
+  },
+  {
+    "revision": "ac4bb356dd45bf60bafd",
+    "url": "/Weather-App/static/js/main.63a14f56.chunk.js"
+  },
+  {
+    "revision": "89bb695666ed5c38f231",
+    "url": "/Weather-App/static/js/runtime~main.671df5a1.js"
+  },
+  {
+    "revision": "1d740f1920ac6c4cece580350bd5412e",
+    "url": "/Weather-App/static/media/154301-minimalism-sunset-water-cloud-water_resources-3840x2160.1d740f19.jpg"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/Weather-App/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/Weather-App/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/Weather-App/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/Weather-App/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ecaf8b481729b18f6a8494d9f691cdae",
+    "url": "/Weather-App/static/media/weathericons-regular-webfont.ecaf8b48.svg"
+  }
+]);
